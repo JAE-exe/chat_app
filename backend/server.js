3380cacdb3692f471552f132d6c8c728164ea84c
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-06-10 23:16:22 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
-app.listen(3000, () => {
+
+app.get("/api/auth/signup", (req, res) => {
+  console.log("Signup");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  console.log("Login");
+});
+app.listen(PORT, () => {
   console.log("Server is running");
 });
